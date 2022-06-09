b74075ca99e8eba8539a839e6d42054bd6311b84
유찬홍 <chanhongy6@naver.com>
2022-06-09 09:59:03 +0900

    UPDATE 코드덩어리

--- 코드덩어리.c
+++ 코드덩어리.c
@@ -2141,18 +2141,16 @@ else if (c > b && b > a) printf("%d", b);*/ //수행평가 힌트
 //	return 0;
 //}
 
-
-
-#include <stdio.h>
-int main()
-{
-int a, b, c;
-scanf("%d %d %d", &a, &b, &c);
-if (a > b && a > c)
-printf("%d", a);
-else if (b > a && b > c)
-printf("%d", b);
-else
-printf("%d", c);
-return 0;
-}
+//#include <stdio.h>
+//int main()
+//{
+//int a, b, c;
+//scanf("%d %d %d", &a, &b, &c);
+//if (a > b && a > c)
+//printf("%d", a);
+//else if (b > a && b > c)
+//printf("%d", b);
+//else
+//printf("%d", c);
+//return 0;
+//}
