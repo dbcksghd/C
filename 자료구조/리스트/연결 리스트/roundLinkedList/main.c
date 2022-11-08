e8a6b6bd7ea9e1625cfe45b9818d3c0a3f52519f
Yoochanhong <chanhongy6@naver.com>
2022-11-08 13:09:50 +0900

    원형 연결 리스트

---
+++ 자료구조/리스트/연결 리스트/roundLinkedList/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ListNode {    // 노드 타입
+    int data;
+    struct ListNode *link;
+} ListNode;
+
+// 리스트의 항목 출력
+void print_list(ListNode *head) {
+    ListNode *p = head->link;
+    do {
+        printf("%d->", p->data);
+        p = p->link;
+    } while (p != head->link);
+}
+
+ListNode *insert_first(ListNode *head, int item) {
+    ListNode *node = (ListNode *) malloc(sizeof(ListNode));
+    node->data = item;
+    if (head == NULL) {
+        head = node;
+        node->link = node;
+    } else {
+        node->link = head->link;
+        head->link = node;
+    }
+    return head;    // 변경된 헤드 포인터를 반환한다.
+}
+
+ListNode *insert_last(ListNode *head, int item) {
+    ListNode *node = (ListNode *) malloc(sizeof(ListNode));
+    node->data = item;
+    if (head == NULL) {
+        head = node;
+        node->link = node;
+    } else {
+        node->link = head->link;
+        head->link = node;
+        head = node;
+    }
+    return head;    // 변경된 헤드 포인터를 반환한다.
+}
+
+ListNode *delete_first(ListNode *head) {
+    ListNode *removed = head->link;
+    if (head == NULL) return NULL;
+    head->link = removed->link;
+    free(removed);
+    return head;
+}
+
+ListNode *delete_last(ListNode *head) {
+    int count = 0;
+    ListNode *removed = head;
+    ListNode *p;
+    for (p = head->link; p != head; p = p->link) {
+        count++;
+    }
+    for (int i = 0; i < count; i++) {
+        p = p->link;
+    }
+    p->link = head->link;
+    head = p;
+    free(removed);
+    return head;
+}
+
+int main(void) {
+    ListNode *head = NULL;
+
+    head = insert_last(head, 20);
+    head = insert_last(head, 30);
+    head = insert_last(head, 40);
+    head = insert_first(head, 10);
+    head = delete_first(head);
+    head = delete_last(head);
+    print_list(head);
+    return 0;
+}
