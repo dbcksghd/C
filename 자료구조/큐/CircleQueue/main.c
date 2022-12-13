46cd1817800cfdc6a34548064aa2bed44f8a4344
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-13 19:19:27 +0900

    원형 큐

--- 자료구조/큐/CircleQueue/main.c
+++ 자료구조/큐/CircleQueue/main.c
@@ -9,20 +9,17 @@ typedef struct queue { // 큐 타입
 
 // 초기화 함수
 void init_queue(QueueType *q) {
-    q->rear = 0;
-    q->front = 0;
+    q->rear = q->front = 0;
 }
 
 // 공백 상태 검출 함수
 int is_empty(QueueType *q) {
-    if (q->front == q->rear) return 1;
-    else return 0;
+    return q->front == q->rear;
 }
 
 // 포화 상태 검출 함수
 int is_full(QueueType *q) {
-    if ((q->rear + 1) % MAX_QUEUE_SIZE == q->front) return 1;
-    else return 0;
+    return (q->rear + 1) % MAX_QUEUE_SIZE == q->front;
 }
 
 
@@ -31,7 +28,7 @@ void enqueue(QueueType *q, int item) {
     if (is_full(q)) printf("큐가 포화상태입니다");
     else {
         q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
-        q->data[++(q->rear)] = item;
+        q->data[q->rear] = item;
     }
 }
 
@@ -39,26 +36,24 @@ void enqueue(QueueType *q, int item) {
 int dequeue(QueueType *q) {
     if (is_empty(q)) {
         printf("큐가 공백상태입니다");
-        return 0;
     } else {
         q->front = (q->front + 1) % MAX_QUEUE_SIZE;
         return q->data[q->front];
     }
 }
 
-// 원형큐 출력 함수
+//원형큐 출력 함수
 void queue_print(QueueType *q) {
-    for (int i = q->front; i != q->rear;) {
+    int i = q->front;
+    while (i != q->rear) {
         i = (i + 1) % (MAX_QUEUE_SIZE);
         printf("%d ", q->data[i]);
     }
     printf("\n");
 }
 
-int main(void) {
+int main() {
     QueueType q;
-    int num;
-
     init_queue(&q);
 
     enqueue(&q, 10);
