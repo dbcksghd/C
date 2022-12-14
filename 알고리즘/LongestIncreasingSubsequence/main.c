ed5d3938ce217c73272c1f01d10e7a8986f70b29
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-14 09:29:04 +0900

    최장 증가 부분 수열

--- 알고리즘/LongestIncreasingSubsequence/main.c
+++ 알고리즘/LongestIncreasingSubsequence/main.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 
 int main() {
-    int n, arr[1000], dp[1000] = {0,}, result = 0;
-    scanf("%d", &n);
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &arr[i]);
-    }
-    for (int i = 0; i < n; i++) {
+    int arr[8] = {90, 10, 20, 50, 30, 70, 10, 30};
+    int dp[8] = {0,}, result = 0;
+    for (int i = 0; i < 8; i++) {
         int c = arr[i];
         for (int j = 0; j < i; j++) {
             if (c > arr[j]) dp[i] = dp[i] > dp[j] ? dp[i] : dp[j];
@@ -15,4 +12,5 @@ int main() {
         if (result < dp[i]) result = dp[i];
     }
     printf("%d", result);
+    //10->20->30->70 = 가장 긴 증가하는 부분 수열은 4
 }
