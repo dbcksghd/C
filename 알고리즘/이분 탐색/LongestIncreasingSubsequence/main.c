55f3af990f97ca824828037c44b0068294c6c715
맥북 유찬홍 <chanhongy6@naver.com>
2023-02-01 01:08:56 +0900

    이분 탐색을 이용한 최장 증가 부분 수열 구하기

--- 알고리즘/이분 탐색/LongestIncreasingSubsequence/main.c
+++ 알고리즘/이분 탐색/LongestIncreasingSubsequence/main.c
@@ -2,21 +2,21 @@
 
 int main() {
     int arr[8] = {90, 10, 20, 50, 30, 70, 10, 30};
-    int bs[8], rs = 0;
+    int bs[8], result = 0;
     bs[0] = arr[0];
     for (int i = 1; i < 8; i++) {
-        if (arr[i] > bs[rs]) bs[++rs] = arr[i];
+        if (arr[i] > bs[result]) bs[++result] = arr[i];
         else {
-            int s = 0, e = rs;
-            while (s < e) {
-                int mid = (s + e) / 2;
-                if (arr[i] > bs[mid]) s = mid + 1;
-                else e = mid;
+            int start = 0, end = result;
+            while (start < end) {
+                int mid = (start + end) / 2;
+                if (arr[i] > bs[mid]) start = mid + 1;
+                else end = mid;
             }
-            int pos = e;
+            int pos = end;
             bs[pos] = arr[i];
         }
     }
-    printf("%d", rs + 1);
+    printf("%d", result + 1);
     return 0;
 }
